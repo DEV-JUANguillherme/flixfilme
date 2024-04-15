45a64b3d72e712960fcd89712d11bce0968cd8e5
Juan Guillherme <130096510+DEV-JUANguillherme@users.noreply.github.com>
2024-04-15 17:09:02 -0300

    adicionando filmes favoritos, tirando da lista e mostrando que nao existe filme na lista

--- src/pages/Favoritos/Favoritos.js
+++ src/pages/Favoritos/Favoritos.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './favoritos.css'
+
 import { json } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
@@ -13,18 +14,27 @@ function Favoritos(){
         setFilmes(JSON.parse(minhaLista) || [])
 
     },[])
+
+    function excluirFilme(id){
+        let filtroFilmes = Filmes.filter((item) => {
+            return(item.id !== id)
+        })
+        setFilmes(filtroFilmes)
+
+        localStorage.setItem('@flixfilme', JSON.stringify(filtroFilmes))
+    }
     return(
         <div className="meus-filmes">
             <h1>MEUS FILMES</h1>
-
+            {Filmes.length === 0 && <span>VOCÊ AINDA NÃO TEM NENHUM FILME SALVO </span>}
             <ul>
                 {Filmes.map((item) => {
                     return(
                         <li key={item.id}>
                             <span>{item.title}</span>
                             <div>
-                                <link to={`/filme/${item.id}`}>ver filmes</link>
-                                <button>Excluir</button>
+                                <Link to={`/filme/${item.id}`}>ver filmes</Link>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
                     )
